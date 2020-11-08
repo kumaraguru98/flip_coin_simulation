18b718459e229d541c189d90c0e9fce541124156
baluguru <smkumarag@gmail.com>
2020-11-08 19:12:22 +0530

    added loop to get multiple times of won

--- flip_coin_simulator.sh
+++ flip_coin_simulator.sh
@@ -1,10 +1,19 @@
 #! /bin/bash -x
 echo "welcome to flipcoinsimulation!!"
-coinrandomcheck=$(( RANDOM%2 ))
-echo $coinrandomcheck
-if (( $coinrandomcheck==0 ))
-then 
-	echo "Head"
-else
-	echo "Tail"
-fi
+headcount=0
+tailcount=0
+
+for (( i=0; i<20; i++ ))
+do
+	coinrandomcheck=$(( RANDOM%2 ))
+	if (( $coinrandomcheck==0 ))
+	then 
+		echo "flips of coin Head"
+		headcount=$(( $headcount+1 ))
+	else
+		echo "flips of Tail"
+		tailcount=$(( $tailcount+1 ))
+	fi
+done
+echo "number of times head won" $headcount
+echo "number of times tail won" $tailcount
