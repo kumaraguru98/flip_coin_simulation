de7b2cb9ed8b6c1ffc1d08365676cdbe056299f2
baluguru <smkumarag@gmail.com>
2020-11-08 19:17:52 +0530

    added code for fliping coin until either of them wins 21times and show winner

--- flip_coin_simulator.sh
+++ flip_coin_simulator.sh
@@ -5,15 +5,28 @@ tailcount=0
 
 for (( i=0; i<20; i++ ))
 do
-	coinrandomcheck=$(( RANDOM%2 ))
-	if (( $coinrandomcheck==0 ))
-	then 
-		echo "flips of coin Head"
-		headcount=$(( $headcount+1 ))
-	else
-		echo "flips of Tail"
-		tailcount=$(( $tailcount+1 ))
+	while [[ $headcount -le 21 && $tailcount -le 21 ]]
+	do
+		coinrandomcheck=$(( RANDOM%2 ))
+		if (( $coinrandomcheck==0 ))
+		then 
+			echo "flips of coin Head"
+			headcount=$(( $headcount+1 ))
+		else
+			echo "flips of Tail"
+			tailcount=$(( $tailcount+1 ))
 	fi
+	done
 done
 echo "number of times head won" $headcount
 echo "number of times tail won" $tailcount
+if [ $headcount -le $tailcount ]
+then
+	echo "won head by "$(($headcount-1))
+elif [ $headcount -le $tailcount ]
+then
+	echo "won tail by "$(($tailcount-1))
+else
+	echo "tie between head and tail"
+fi
+
