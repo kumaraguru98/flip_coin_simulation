9821236827f8c7c9a6e1d1ce8123e89018faa692
baluguru <smkumarag@gmail.com>
2020-11-08 19:25:15 +0530

    added code for continuing game till minimum difference of 2points achieved

--- flip_coin_simulator.sh
+++ flip_coin_simulator.sh
@@ -1,25 +1,25 @@
 #! /bin/bash -x
 echo "welcome to flipcoinsimulation!!"
+minimumacheived() {
 headcount=0
 tailcount=0
-
-for (( i=0; i<20; i++ ))
+while [[ $headcount -le 21 && $tailcount -le 21 ]]
 do
-	while [[ $headcount -le 21 && $tailcount -le 21 ]]
-	do
-		coinrandomcheck=$(( RANDOM%2 ))
-		if (( $coinrandomcheck==0 ))
-		then 
-			echo "flips of coin Head"
-			headcount=$(( $headcount+1 ))
-		else
-			echo "flips of Tail"
-			tailcount=$(( $tailcount+1 ))
+	coinrandomcheck=$(( RANDOM%2 ))
+	if (( $coinrandomcheck==0 ))
+	then 
+		echo "flips of coin Head"
+		headcount=$(( $headcount+1 ))
+	else
+		echo "flips of Tail"
+		tailcount=$(( $tailcount+1 ))
 	fi
-	done
 done
 echo "number of times head won" $headcount
 echo "number of times tail won" $tailcount
+}
+difference=$(( $headcount - $tailcount ))
+minimumachieved
 if [ $headcount -le $tailcount ]
 then
 	echo "won head by "$(($headcount-1))
@@ -28,5 +28,9 @@ then
 	echo "won tail by "$(($tailcount-1))
 else
 	echo "tie between head and tail"
+while [[  $difference -le 2 ]]
+   do
+	minimumachieved
+   done
 fi
 
